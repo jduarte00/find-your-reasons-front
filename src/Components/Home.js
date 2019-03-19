b195cf47bddcf7a483ae8b94029a3bc9684fa93e
Jose Duarte <dumejo92@gmail.com>
2019-03-18 23:03:29 -0600

    refactor Home to include components

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,60 +1,14 @@
 import React from "react";
-
-import { Button, Icon } from "antd";
+import Hero from "./HomeComponents/Hero";
+import Services from "./HomeComponents/Services";
+import MainChart from "./HomeComponents/MainChart";
 
 export default function Home() {
   return (
     <div>
-      <div className="heroReasons">
-        <div className="logo">
-          <img
-            src="http://www.stickpng.com/assets/images/584d4b6e0a44bd1070d5d493.png"
-            alt="logo"
-            className="mainLogo"
-          />
-        </div>
-        <div className="mainDescription">
-          <h1 className="appMainName">Find your reasons</h1>
-          <div className="appMainDescription">
-            <p>
-              We think every business have the potential to be sucessfull, you
-              just need the right intel...
-            </p>
-          </div>
-          <div className="mainButtonSection">
-            <Button shape="round"> Our offer to you</Button>
-            <Button shape="round"> Create an account with us</Button>
-          </div>
-        </div>
-      </div>
-      <div className="ourMainServices">
-        <div className="ant-row">
-          <div className="ant-col-md-6 ant-col-xs-24 card-services">
-            <Icon type="dashboard" style={{ fontSize: "2rem" }} />
-            <h3>Acid Reason</h3>
-            <hr />
-            <p>Would you like to know how fuck your organization is?</p>
-          </div>
-          <div className="ant-col-md-6 ant-col-xs-24 card-services">
-            <Icon type="dashboard" style={{ fontSize: "2rem" }} />
-            <h3>Acid Reason</h3>
-            <hr />
-            <p>Would you like to know how fuck your organization is?</p>
-          </div>
-          <div className="ant-col-md-6 ant-col-xs-24 card-services">
-            <Icon type="dashboard" style={{ fontSize: "2rem" }} />
-            <h3>Acid Reason</h3>
-            <hr />
-            <p>Would you like to know how fuck your organization is?</p>
-          </div>
-          <div className="ant-col-md-6 ant-col-xs-24 card-services">
-            <Icon type="dashboard" style={{ fontSize: "2rem" }} />
-            <h3>Acid Reason</h3>
-            <hr />
-            <p>Would you like to know how fuck your organization is?</p>
-          </div>
-        </div>
-      </div>
+      <Hero />
+      <Services />
+      <MainChart />
     </div>
   );
 }
