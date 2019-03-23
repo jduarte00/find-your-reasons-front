7a6ca199de69794f2601218fc24ec5a66cb2395b
Jose Duarte <dumejo92@gmail.com>
2019-03-23 10:06:49 -0600

    Move the panel component to an individual folder

--- src/Components/Layout/Panel/MainPage.js
+++ src/Components/Layout/Panel/MainPage.js
@@ -1,24 +1,24 @@
 import React, { Component } from "react";
-import MyBreadcrumb from "./Breadcrumb";
-import { Table } from "antd";
-import { Bar } from "react-chartjs-2";
+import MyBreadcrumb from "../Breadcrumb";
+import { Table, Card } from "antd";
+import { Bar, Pie } from "react-chartjs-2";
 import axios from "axios";
 
 const data = {
   labels: ["Incomes", "Expenses", "Revenue", "Taxes"],
   datasets: [
     {
-      label: "Proyect 1",
+      label: "App 1",
       data: [617594, 181045, 153060, 106519],
       backgroundColor: "rgba(255, 99, 132, 0.6)"
     },
     {
-      label: "Proyect 2",
+      label: "App 2",
       data: [417594, 281045, 753060, 306519],
       backgroundColor: "rgba(0, 99, 132, 0.6)"
     },
     {
-      label: "Proyect 3",
+      label: "App 3",
       data: [217594, 681045, 353060, 906519],
       backgroundColor: "rgba(100, 99, 132, 0.6)"
     }
@@ -29,8 +29,9 @@ export default class MainPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      registeredCompanies: null,
-      user: props.theUser
+      userApps: null,
+      user: props.theUser,
+      key: "User Score"
     };
 
     this.columns = [
@@ -44,12 +45,12 @@ export default class MainPage extends Component {
         dataIndex: "view",
         align: "right",
         key: "view",
-        render: id => (
+        render: theId => (
           <div className="field has-addons" id="center-buttons-table">
-            <p className="control">
+            <p className="control" id="remove-margin-buttons">
               <a
                 className="button is-success is-small"
-                href={`http://localhost:3001/viewcompany/${id}`}
+                href={`http://localhost:3001/viewcompany/${theId}`}
               >
                 <span className="icon is-small">
                   <i className="fas fa-align-left" />
@@ -57,10 +58,10 @@ export default class MainPage extends Component {
                 <span>View</span>
               </a>
             </p>
-            <p className="control">
+            <p className="control" id="remove-margin-buttons">
               <a
                 className="button is-info is-small"
-                href={`http://localhost:3001/editcompany/${id}`}
+                href={`http://localhost:3001/editcompany/${theId}`}
               >
                 <span className="icon is-small">
                   <i className="fas fa-align-center" />
@@ -68,10 +69,13 @@ export default class MainPage extends Component {
                 <span>Edit</span>
               </a>
             </p>
-            <p className="control">
+            <p className="control" id="remove-margin-buttons">
               <a
                 className="button is-danger is-small"
-                href={`http://localhost:3001/deletecompany/${id}`}
+                onClick={(e, Id = theId) => {
+                  console.log(Id, "this is theID", e);
+                  this.deleteApp(Id);
+                }}
               >
                 <span className="icon is-small">
                   <i className="fas fa-align-right" />
@@ -83,39 +87,76 @@ export default class MainPage extends Component {
         )
       }
     ];
+
+    this.tabList = [
+      {
+        key: "User Score",
+        tab: "user score"
+      },
+      {
+        key: "Second Metric",
+        tab: "second metric"
+      },
+      {
+        key: "Third Metric",
+        tab: "third metric"
+      }
+    ];
+
+    this.contentList = {
+      "User Score": <p className="title"> 659</p>,
+      "Second Metric": <p className="title">234</p>,
+      "Third Metric": <p className="title">789</p>
+    };
   }
 
   getRegisteredCompanies = () => {
     axios
       .get("http://localhost:3001/user/panel", { withCredentials: true })
       .then(theData => {
-        this.setState({ registeredCompanies: theData.data });
+        console.log(theData, "theData");
+        this.setState({ userApps: theData.data });
         return theData.data;
       });
+  };
+
+  onTabChange = (key, type) => {
+    this.setState({ [type]: key });
   };
 
   componentDidMount = () => {
     this.getRegisteredCompanies();
   };
 
+  deleteApp = appid => {
+    console.log(appid, "this is fckn appid");
+    axios
+      .delete(`http://localhost:3001/app/deleteapp/${appid}`, {
+        withCredentials: true
+      })
+      .then(theIndex => {
+        this.getRegisteredCompanies();
+      });
+  };
+
   render() {
     console.log(this.state);
     return (
       <div>
-        <div className="container">
+        <div className="container responsive-padding-sides">
           <MyBreadcrumb routes={[this.state.user.username, "home"]} />
-          <div className="columns">
-            <div className="column ">
-              {this.state.registeredCompanies ? (
+          <div className="columns margin-top">
+            <div className="column">
+              {this.state.userApps ? (
                 <Table
                   columns={this.columns}
-                  dataSource={this.state.registeredCompanies.tableData}
+                  dataSource={this.state.userApps.tableData}
                   size="middle"
                 />
               ) : null}
             </div>
           </div>
-          <div className="columns">
+          <div className="columns margin-top">
             <div className="column">
               <p class="heading">Total Income</p>
               <p class="title">3,456</p>
@@ -133,12 +174,55 @@ export default class MainPage extends Component {
               <p class="title">3,456</p>
             </div>
           </div>
-          <div className="columns">
+          <div className="columns margin-top">
             <div className="column">
               <Bar
                 data={data}
                 options={{
-                  mantainAspectRatio: true,
+                  mantainAspectRatio: false,
+                  scales: {
+                    yAxes: [
+                      {
+                        ticks: {
+                          callback: function(value, index, values) {
+                            if (parseInt(value) >= 1000) {
+                              return (
+                                "$" +
+                                value
+                                  .toString()
+                                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+                              );
+                            } else {
+                              return "$" + value;
+                            }
+                          }
+                        }
+                      }
+                    ]
+                  }
+                }}
+              />
+            </div>
+          </div>
+          <div className="columns margin-top">
+            <div className="column is-half">
+              <Card
+                style={{ height: "100%" }}
+                title="Main Metrics"
+                tabList={this.tabList}
+                activeTabKey={this.state.key}
+                onTabChange={key => {
+                  this.onTabChange(key, "key");
+                }}
+              >
+                {this.contentList[this.state.key]}
+              </Card>
+            </div>
+            <div className="column is-half margin-top-responsive">
+              <Pie
+                data={data}
+                options={{
+                  mantainAspectRatio: false,
                   scales: {
                     yAxes: [
                       {
